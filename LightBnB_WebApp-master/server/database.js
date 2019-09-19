d8bfbcacdb5becf63ff5eb165ce03f68e4afca55
javascript01fan <ejrojas@icloud.com>
2019-09-19 19:14:26 -0400

    get all reservations fuction working propertly

--- LightBnB_WebApp-master/server/database.js
+++ LightBnB_WebApp-master/server/database.js
@@ -10,7 +10,18 @@ const db = new Client({
   database: "lightbnb"
 });
 
-
+/* const dbQuery = `SELECT properties.*, reservations.*, avg(rating) as average_rating
+FROM reservations
+JOIN properties ON reservations.property_id = properties.id
+JOIN property_reviews ON properties.id = property_reviews.property_id 
+WHERE reservations.guest_id = 1
+AND reservations.end_date < now()::date
+GROUP BY properties.id, reservations.id
+ORDER BY reservations.start_date
+LIMIT 2`;
+db.query(dbQuery).then(res => {
+  console.log(res.rows)
+}).catch(err=> console.log('Error getting reservations',err)) */
 
 db.connect(err => {
   if (err) {
@@ -86,7 +97,21 @@ exports.addUser = addUser
  * @return {Promise<[{}]>} A promise to the reservations.
  */
 const getAllReservations = function(guest_id, limit = 10) {
-  return getAllProperties(null, 2);
+  // return getAllProperties(null, 2);
+  const dbQuery = `SELECT properties.*, reservations.*, avg(rating) as average_rating
+  FROM reservations
+  JOIN properties ON reservations.property_id = properties.id
+  JOIN property_reviews ON properties.id = property_reviews.property_id 
+  WHERE reservations.guest_id = $1
+  AND reservations.end_date < now()::date
+  GROUP BY properties.id, reservations.id
+  ORDER BY reservations.start_date
+  LIMIT $2`;
+  const values = [guest_id,limit]
+  return db.query(dbQuery,values).then(res => {
+    console.log(res.rows)
+    return res.rows
+  }).catch(err=> console.log('Error getting reservations',err))
 };
 exports.getAllReservations = getAllReservations;
 
